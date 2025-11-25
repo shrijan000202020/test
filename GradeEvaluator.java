0080b2187db4c700aa6163b0223e33cae88b89b7
shrijan kc <skc265789@gmail.com>
2025-11-25 12:59:01 +0545

    ladder

--- GradeEvaluator.java
+++ GradeEvaluator.java
@@ -6,12 +6,40 @@ public class GradeEvaluator{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your marks");
         marks = scanner.nextInt();
-        if(marks >=40){
-            System.out.println("Passed");
+        if(marks >=40)
+        {
+            if(marks >=90)
+            {
+                System.out.println("A+");
+            }
+            else if(marks>=80)
+        {
+            System.out.println("A");
+        }
+        else if(marks>=70)
+        {
+            System.out.println("B+");
+        }
+        else if(marks>=60)
+        {
+            System.out.println("B");
+        }
+        else if(marks>=50)
+        {
+            System.out.println("C+");
         }
         else
+            {
+                System.out.println("C");
+            }
+        }    
+        else if(marks>100)
         {
-            System.out.println("Failed");
+            System.out.println("invalid");
         }
-    }
-}
+        else
+            {
+                System.out.println("fail");
+            }
+        }
+ }
